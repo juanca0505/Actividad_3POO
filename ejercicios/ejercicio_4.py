e17baae3953bd7c5cdd45b8c030e07fe250925c6
SkynetProject <o.lamprea99gmail.com>
2023-08-08 13:24:14 -0500

    Initial commit

--- ejercicios/ejercicio_4.py
+++ ejercicios/ejercicio_4.py
@@ -1,2 +1,26 @@
 class Rectangulo:
-    def __init__(self, esquina_sup_izquierda, esquina_inf_der):
+    def __init__(self, esquina_sup_izq, esquina_inf_der):
+        self.esquina_sup_izq = esquina_sup_izq
+        self.esquina_inf_der = esquina_inf_der
+
+    def calcular_perimetro(self):
+        base = abs(self.esquina_sup_izq.x - self. esquina_inf_der.x)
+        altura = abs(self.esquina_sup_izq.y - self.esquina_inf_der.y)
+        perimetro = 2 * (base + altura)
+        return perimetro
+
+    def calcular_area(self):
+        base = abs(self.esquina_sup_izq.x - self.esquina_inf_der.x)
+        altura = abs(self.esquina_sup_izq.y - self.esquina_inf_der.y)
+        area = base * altura
+
+    def es_cuadrado(self):
+        base = abs(self.esquina_sup_izq.x - self.esquina_inf_der.x)
+        altura = abs(self.esquina_sup_izq.y - self.esquina_inf_der.y)
+        return base == altura
+
+
+
+
+
+
