d008f3dbcfe444891d890cb1cfcda528d4eec1b5
SkynetProject <o.lamprea99gmail.com>
2023-08-08 13:42:27 -0500

    Initial commit

--- ejercicios/ejercicio_5.py
+++ ejercicios/ejercicio_5.py
@@ -1,1 +1,16 @@
+import math
 class Ciruclo:
+    def __init__(self, centro, radio):
+        self. centro = centro
+        self. radio = radio
+    def calcular_area(self):
+        area = math.pi * self.radio**2
+        return area
+
+    def calcular_perimetro(self):
+        perimetro = 2 * math.pi * self.radio
+        return perimetro
+
+    def punto_pertenece(self, punto):
+        distancia = self.centro.calcular_distancia(punto)
+        return distancia <= self.radio
